54f555f5f4ec277c61faa7005f3e8bf0f2089549
isaacchircop <isaacchircop@gmail.com>
2014-07-11 17:01:35 +0200

    Made username clickable

--- twitterlite-app/Scripts/app.js
+++ twitterlite-app/Scripts/app.js
@@ -4,7 +4,7 @@ var limit = 25;
 $(document).ready(function(){
 	$.get("http://localhost:8080/twitterlite/messages", { offset:offset, limit:limit }, function(data){
 		for (var i = 0; i < data.length; i++) {
-		    $("#tweet-container").append("<div class='panel panel-default'><div class='panel-body'><b class='red'>@" + data[i].username + ":</b> " + data[i].content + "</div></div>");
+		    $("#tweet-container").append("<div class='panel panel-default'><div class='panel-body'><b class='red'><span id=\"userMention\" onClick=\"searchUser\">@" + data[i].username + ":</span></b> " + data[i].content + "</div></div>");
 		}
 
 		offset = offset + limit;
@@ -23,24 +23,55 @@ $(document).ready(function(){
 	});
 
 	$("#button").click(function(){
-		var username = $("#username").text();
-		var content = $("#content").text();
+	
+		var valid = true;
+	
+		var username = $("#username").val();
+		var content = $("#content").val();
 		
-		$.ajax({
-			url: 'http://localhost:8080/twitterlite/tweets',
-			type: 'POST',
-			data: {
-			
-				username: $("#username").val(),
-				content: $("#content").val()
-			
-			},
-			success: function(data){
-			
-				console.log("OK");
+		if (username == "") {
+		
+			valid = false;
+			$("#usernameError").text("* Empty Username Field");
+		
+		}
+		
+		if (content == "") {
+		
+			valid = false;
+			$("#messageError").text("* Empty Content Field");
+		
+		}
+		
+		if (valid == true) {
+		
+			$.ajax({
+				url: 'http://localhost:8080/twitterlite/tweets',
+				type: 'POST',
+				data: {
+				
+					username: $("#username").val(),
+					content: $("#content").val()
+				
+				},
+				success: function(data){
+				
+					console.log("OK");
+				
+				}
+			});
 			
 			}
-		});
 		
 	});
+	
+	fucntion searchUser() {
+	
+		console.log("searching user");
+	
+	}
+	
+	
+	
+	
 });
