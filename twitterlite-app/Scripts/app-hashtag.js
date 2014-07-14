b4b39cc38c8a93240ee72a7d5f71bd446082c879
Maria Camenzuli <maria.camenzuli@ixaris.com>
2014-07-14 10:31:06 +0200

    Added hashtag page functionality.

---
+++ twitterlite-app/Scripts/app-hashtag.js
@@ -0,0 +1,44 @@
+var offset = 0;
+var limit = 25;
+
+function appendTweet(tweet){
+	for (var i = 0; i < tweet.hashtags.length; i++) {
+		var regex = new RegExp("#" + tweet.hashtags[i],"i");
+	    var htMatches = tweet.content.match(regex);
+	   
+		tweet.content = tweet.content.substr(0, htMatches.index) + "<a class='hashtag'>#" + tweet.hashtags[i] + "</a>" + tweet.content.substr(tweet.hashtags[i].length + htMatches.index + 1);
+	    
+	}
+
+	$("#tweet-container").append("<div class='panel panel-default'><div class='panel-body'><b class='red'><a class='userMention'>@" + tweet.username + ":</a></b> " + tweet.content + "</div></div>");
+}
+
+$(document).ready(function(){
+	$.get("http://localhost:8080/twitterlite/messages/hashtags/" + window.location.search.substr(1), { offset:offset, limit:limit }, function(data){
+		for (var i = 0; i < data.length; i++) {
+		   appendTweet(data[i]);
+		}
+
+		offset = offset + limit;
+	});
+
+	$("#moreTweets").click(function(){
+		$.get("http://localhost:8080/twitterlite/messages/hashtags/" + window.location.search.substr(1), { offset:offset, limit:limit }, function(data){
+			if (data.length != 0){
+				for (var i = 0; i < data.length; i++) {
+				    appendTweet(data[i]);
+				}
+
+				offset = offset + limit;
+			}
+		});
+	});
+	
+	$("#tweet-container").on("click", ".userMention", function() {
+		console.log("Searching");
+	})
+	$("#tweet-container").on("click", ".hashtag", function(data) {
+		window.location.href = "hashtag.html?" + data.currentTarget.innerText.substr(1);
+	})
+
+});
